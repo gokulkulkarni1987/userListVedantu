6a8de4af0fed1f50e764d8c7fb99f5a94ea4c793
Gokul Kulkarni <gokul.kulkarni@yasco.in>
2019-06-04 16:39:03 +0530

    fetching user and storing done

--- src/home/HomeScreen.js
+++ src/home/HomeScreen.js
@@ -1,15 +1,58 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { connect } from "react-redux";
+import {
+  View,
+  TouchableWithoutFeedback,
+  FlatList
+} from 'react-native';
+import { Text, Card } from 'react-native-elements';
+import {
+  fetchUsers
+} from './actions/UserActions';
 
 class HomeScreen extends Component {
 
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderItem({ item }) {
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.itemClicked(item)}
+      >
+        <Card
+          title={item.title}
+          image={{
+            uri: item.thumbnailUrl
+          }}
+        />
+      </TouchableWithoutFeedback>
+    );
+  }
+
+  keyExtractor(item) {
+    return `item-${item.id}`;
+  }
+
   render() {
+    console.log('this.props.', this.props);
     return (
       <View>
-
+        <FlatList
+          data={this.props.users}
+          renderItem={this.renderItem.bind(this)}
+          keyExtractor={this.keyExtractor.bind(this)}
+        />
       </View>
     )
   }
 }
 
-export default HomeScreen;
+const mapStateToProps = ({ users }) => {
+  return { ...users };
+};
+
+export default connect(mapStateToProps, {
+  fetchUsers
+})(HomeScreen);
--- src/home/reducers/UserReducer.js
+++ src/home/reducers/UserReducer.js
@@ -5,6 +5,7 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
   let newState = state;
+  console.log('this is called', action);
 
   switch(action.type) {
     case "FETCH_USERS_SUCCESS":
