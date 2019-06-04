0291d20bf56474ca5af7a64fb82933ca0de16937
Gokul Kulkarni <gokul.kulkarni@yasco.in>
2019-06-04 18:59:34 +0530

    code refactoring: removed logs

--- src/home/reducers/UserReducer.js
+++ src/home/reducers/UserReducer.js
@@ -9,7 +9,6 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log('yhis is the state: ', state);
   let newState = state;
   switch(action.type) {
     case "FETCH_USERS_SUCCESS":
